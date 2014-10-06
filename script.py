6254f534492878df01ae738176d35cca37691d63
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-10-06 18:41:35 +0400

    Add check-changes option to script

--- script.py
+++ script.py
@@ -2,6 +2,7 @@
 
 import argparse
 import os
+import sys
 
 parser = argparse.ArgumentParser('Generate source files: A.cpp')
 parser.add_argument('--dir', help='Output directory', required=True)
@@ -10,6 +11,11 @@ parser.add_argument(
     action='store_true',
     help='Do not create file if already exists and content is the same'
 )
+parser.add_argument(
+    '--check-changes',
+    action='store_true',
+    help='Check that some of the files really change'
+)
 args = parser.parse_args()
 
 print('Generate (python script)')
@@ -17,11 +23,15 @@ print('Generate (python script)')
 outdir = args.dir
 os.makedirs(outdir, exist_ok=True)
 
+change = False
+
 def create(filename, content):
   if os.path.exists(filename) and args.smart:
     old_content = open(filename, 'r').read()
     if old_content == content:
       return
+  global change
+  change = True
   open(filename, 'w').write(content)
 
 # Create A.cpp
@@ -32,3 +42,7 @@ const char* A() {
 """
 
 create(os.path.join(outdir, 'A.cpp'), a_content)
+
+# Check changes
+if (not change) and args.check_changes:
+  sys.exit('No changes!')
