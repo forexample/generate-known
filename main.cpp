fe72db67e0380e9671a83ef0cf5615d994dedb5b
Ruslan Baratov <ruslan_baratov@yahoo.com>
2014-10-06 18:46:39 +0400

    Generate A1.cpp and A2.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
-const char* A();
+const char* A1();
+const char* A2();
 
 int main() {
-  std::cout << "A say: " << A() << std::endl;
+  std::cout << "A1 say: " << A1() << std::endl;
+  std::cout << "A2 say: " << A2() << std::endl;
 }
